f0cc210a51a8f370860088415f09955dc965874f
'Jacksonchoii' <'cccctae@gmail.com'>
2020-11-13 18:04:31 +0900

    수정

--- src/com/javaex/practice/Ex11.java
+++ src/com/javaex/practice/Ex11.java
@@ -23,9 +23,30 @@ public class Ex11 {
 			case "i":
 			case "o":
 			case "u": System.out.println("모음입니다.");
-			break;
-			
-			default: System.out.println("자음입니다.");
+					  break;
+			case "b":
+			case "c":
+			case "d":
+			case "f":
+			case "g":
+			case "h":
+			case "j":
+			case "k":
+			case "l":
+			case "m":
+			case "n":
+			case "p":
+			case "q":
+			case "r":
+			case "s":
+			case "t":
+			case "v":
+			case "w":
+			case "x":
+			case "y":
+			case "z": System.out.println("자음입니다.");
+					  break;
+			default: System.out.println("다시 입력해주세요.");
 			break;
 		}
 		
